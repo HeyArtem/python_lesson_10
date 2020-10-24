ad6c15ae0710208ffa299e16bee59816799ce5e7
HeyArtem <artem_white@mail.ru>
2020-10-24 19:15:55 +0300

    10.2 тестирование классов pytest

--- test_pytest_dices.py
+++ test_pytest_dices.py
@@ -1,4 +1,4 @@
-import pytest
+
 
 '''
 Это тоже глава 10.2 тестирование классов pytest
@@ -6,57 +6,103 @@ import pytest
 
 
 Артем!!! Запомни сколько ты потерял часов, пока вспомнил, 
-что страница должна называться со слова test
+что страница должна называться со слова test и в терминале писать слово pytest
 
 '''
 
-
+import pytest
 from Тестирование102классов_Pytest import Dice_incapsulation
 # для написания тестов создадим спецю класс
 
-class TestDice_pytest: # когда пишем урок incapsulation изменили название класса с TestDice на TestDice_pytest
+class TestDice: # когда пишем урок incapsulation изменили название класса с TestDice на TestDice_pytest
 
     def setup(self): # позже обьяснят, для чего эти функции
-
-        '''
-        Так, как в каждой функции идет дублирование кода
-        например:
+        ''''
+        Зачем setup?
+        В каждо функции мы повторяем код
+          dice_game = Dice_incapsulation(2)
+        что бы не повторять, мы создаем обьект
         dice_game = Dice_incapsulation(2)
-
-        Мы создали объект self.dice_game, который является атрибутом класса setup
-        Теперь во всем коде мы можем обращаться к обьекту dice_game через атрибут self.dice_game
-
-
+        (причем пишем self.dice_game т.к.это атрибут класса class TestDice и везде ниже дапишем слово self.)
+         -> в кажд фу-ции удаляем его(до этого в каждой функции была запись dice_game = Dice_incapsulation(2)
+        и еще просто запринтим StartTest
         '''
-        self.dice_game = Dice_incapsulation(3)
+        self.dice_game = Dice_incapsulation(2)
         print('Start test!') # просто так запрнтил
 
-    def teardown(self): # teardown то, что выполняется после завершения
-        self.dice_game.current_throw = 0 # мы можем например, каждый раз обновлять current_throw
-        print('Test has done!')
+
+    def teardown(self):
+        ''''
+        teardown то, что выполняется после завершения тестирования,
+        например это может быть удаление обьектов, очистка памяти
+        или отключение соединение базы данных с сервреом
+
+        Для примера включю обнуление счетчика бросков current_throw
+        '''
+        self.dice_game.current_throw == 0
+        print('Test has done! Короче закончен')
 
 
     def test_init(self): # протестируем init
-        # dice_game = Dice_incapsulation(2) # тестируем self.throw_num = N  Броски. Закоментил, т.к. использую def setup
-        assert self.dice_game.throw_num == 3
-        assert self.dice_game.current_throw == 0 # текущий бросок
+        assert self.dice_game.throw_num == 2
+        ''''
+        Тест 1
+        ПРотестируем init, убедимся, что когда передаем значеи N,
+        параметр throw_num = N
+        '''
+
+        assert self.dice_game.current_throw == 0
+        ''''
+        Тест 2
+        тестируем счетчик бросков
+        '''
+
 
     def test_dice_setter(self):
-        # dice_game = Dice_incapsulation(3) #Закоментил, т.к. использую def setup
+        ''''
+        Тест 3
+        Протестируем функцию присваивания через setter
+        '''
         self.dice_game.hidden_num_1 = 5
         self.dice_game.hidden_num_2 = 5
         assert (self.dice_game.hidden_num_1 == 5) & (self.dice_game.hidden_num_2 == 5)
-
+        ''''Сложное условие, что первый и второй будут 5 одновременно'''
 
         with pytest.raises(ValueError):
-            # код вызывающий данную ошибку
-            self.dice_game.hidden_num_1 = 8
-
+            ''''
+            Тест4
+            Спец. ключевое слово, похоже на менеджер контекста
+            слово with
+            Протестируем фу-ю присваивания которую мы написали с помощью Сетера
+            ValueError ошибка которую пытаемся поймать
+            Напишем например = 8, это выходит за пределы (dice > 0) & (dice < 7)
+            
+            !!!  У меня почему то система не выполняет этот тест, не важно какое число стоит !!!
+            
+            Так же мы можем написать в место ValueError -> TypError и тест будет провален 
+            '''
+            self.dice_game.hidden_num_1 = 200000 # ну не работает!!
     def test_throw_dices(self):
-        # dice_game = Dice_incapsulation(3) Закоментил, т.к. использую def setup
+        '''
+        Тест 5
+        ПРотестируем функцию throw_dices.
+        Убедимся, что счетчик уведичивается с каждым броском
+        '''
         self.dice_game.set_hidden_numbers()
-        self.dice_game.throw_dices()
+        self.dice_game.throw_dices() # это бросили кости
         assert self.dice_game.current_throw == 1
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
