19159cd282711444d0c43c6ba1ae04e2bd851826
HeyArtem <artem_white@mail.ru>
2020-10-28 00:26:32 +0300

    Изучаю pytest от Эльдара ч.1-> просто функция (я ее тстирую). Сначала для двух имен, дальше переписал для трех имен

--- name_function.py
+++ name_function.py
@@ -3,12 +3,29 @@
 Здесь пишу тестируемую функцию
 '''
 
-def get_formatted_name(first, last):
-    ''''
-    Функция, получает имя и фамилию и возвращает
-    отформатированное полное имя (первая буква имени и фамилии будет Заглавной
-    '''
-    full_name = f"{first} {last}"
+# def get_formatted_name(first, last):
+#     ''''
+#     Функция, получает имя и фамилию и возвращает
+#     отформатированное полное имя (первая буква имени и фамилии будет Заглавной
+#     '''
+#     full_name = f"{first} {last}"
+#     return full_name.title()
+
+# name_good = get_formatted_name('фЕрДинАНт', 'бЕКкеР')
+# print(name_good)
+
+''''
+Сбой теста
+(ч.3 стр test_name_function)
+
+Изменим функцию get_formatted_name(), чтобы она работала с вторыми именами. 
+Сделаем это так, чтобы она перестала работать с полными именами из двух компонентов. 
+Заменим содержимое файла name_function.py следующим кодом:
+'''
+def get_formatted_name(first, middle, last):
+    """
+    Строит отформатированное полное имя.
+    Эта версия должна работать для полных имен из трех компонентов.
+    """
+    full_name = f"{first} {middle} {last}" # добавили второе имя
     return full_name.title()
-name_good = get_formatted_name('фЕрДинАНт', 'бЕКкеР')
-print(name_good)
