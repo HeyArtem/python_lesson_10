eb658373d669b7ddcfec0edb7d4e4ea0d3645352
HeyArtem <artem_white@mail.ru>
2020-10-30 21:02:25 +0300

    Изучаю pytest от Эльдара ч.5 Запускаю тест анонимного опроса

---
+++ language_survey.py
@@ -0,0 +1,22 @@
+''''
+Изучаю pytest от Эльдара ч.5
+'''
+
+from survey import AnonymousSurvey
+
+# Определение вопроса с созданием экземпляра AnonymousSurvey.
+question = "What language did you first learn to speak\nНа каком языке вы впервые научились говорить?"
+my_survey = AnonymousSurvey(question)
+
+# Вывод вопроса и сохранение ответов.
+my_survey.show_question()
+print("Enter 'q' at any time to quit.\n")
+while True:
+    response = input("Language: ")
+    if response == 'q':
+        break
+    my_survey.store_response(response)
+
+# Вывод результатов опроса.
+print("\nThank you to everyone who participated in the survey!")
+my_survey.show_results()
