43239d0aaf9cb91443c3b4c65110723848541f3d
HeyArtem <artem_white@mail.ru>
2020-11-02 02:42:01 +0300

    Изучаю pytest от Эльдара ч.6
    Тестю класс анонимного опроса. Сначало на один ответ, потом не несколько ответов..

--- test_survey.py
+++ test_survey.py
@@ -4,10 +4,13 @@ from survey import AnonymousSurvey # импортирование тестиру
 
 ''''
 Изучаю pytest от Эльдара ч.6
+Напишем тест, который проверяет всего один аспект поведения AnonymousSurvey: 
+ один ответ на опрос сохраняется правильно. Сохраним в файл test_survey.py следующий код:
 '''
 
 class TestAnonmyousSurvey(unittest.TestCase):  # сценарий наследуется от unittest.TestCase
     """Тесты для класса AnonymousSurvey"""
+
 
     def test_store_single_response(self):  # проверяет, что сохраненный ответ действительно попадает в список ответов опроса; имя начиниется с test_
         """Проверяет, что один ответ сохранен правильно."""
@@ -16,31 +19,35 @@ class TestAnonmyousSurvey(unittest.TestCase):  # сценарий наследу
         my_survey.store_response('English')  # сохраняем один ответ (English)
         self.assertIn('English', my_survey.responses)  # проверяет наличие тестового значения в списке ответов.
 
-'''
-Тест проверяет электронный опрос только на одно имя.
-Допишем метод, который будет проверять несколько имен
-'''
+    '''
+    Тест проверяет электронный опрос только на одно имя.
+    Допишем метод, который будет проверять несколько имен
+    '''
 
     def test_store_three_responses(self):
-        '''' Проверяет, что три ответа были сохранены правильно. '''
+        """Проверяет, что три ответа были сохранены правильно."""
         question = "What language did you first learn to speak?"
-        my_syrvey = AnonymousSurvey(question)
+        my_survey = AnonymousSurvey(question)
         responses = ['English', 'Spanish', 'Mandarin']
         for response in responses:
-            my_syrvey.store_response(response)
+            my_survey.store_response(response)
         for response in responses:
-            self.assertIn(response, my_syrvey.responses)
+            self.assertIn(response, my_survey.responses)
 
 
 
-    ''''
-    
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
 
-'''
+
 if __name__ == '__main__':
     unittest.main()
 
-''''ПОКА ТЕСТ НЕ ИДЕТ!!!!!          '''
+''''
+Дальше я создам страницу test_2_survey,
+в котрой буду использовать метод seUp
+он позволит сразу создать экземпляр  AnonymousSurvey (my_survey = AnonymousSurvey(question))
+и так же создать набор ответов которые дальше будут использоваться в методах  test_store_single_response() и test_store_three_responses().
+
+Т.е. мне не нужно будет писать (создавать) в начале каждого метода my_survey = AnonymousSurvey(question)
+и писать вопрос и варианты ответов
+'''
+
